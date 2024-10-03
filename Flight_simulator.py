af83eea9a7f0186f62dee3f5339c3c66a087e35b
aRedBoss <aldulaimiomar111@gmail.com>
2024-10-03 14:12:25 +0300

    py

--- Flight_simulator.py
+++ Flight_simulator.py
@@ -1,25 +1,16 @@
 import level_function
 import function
 import time
-import scoreboard
 
-name = input("Enter your name: ")
-show_scoreboard = input("Would you like to see the scoreboard? (y/n): ").strip().lower()
-if show_scoreboard == 'y':
-    scoreboard.display_scoreboard()
-
-function.add_player(name)
+name = input("name ")
+function.default_settings(name)
 
 while True:
 
-    try:
-        level_function.level_one(name)
-        level_function.level_two(name)
-        points = function.get_points(name)
-        print(f"You have {points} points")
-        print("You win!")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        time.sleep(3)
-        break 
+    level_function.level_one(name)
+    level_function.level_two(name)
+    points = function.get_points(name)
+    print(f"You have {points} points")
+    print("You win!")
+    time.sleep(3)
+    break
